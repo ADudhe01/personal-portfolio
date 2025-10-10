150d606ed740e1332173a7f82e01514c2ced18ca
ADudhe01 <atharva.dudhe@gmail.com>
2025-10-11 00:01:14 +1100

    chore: build to docs for GH Pages and navbar alignment fixes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/personal-portfolio/",
   plugins: [react()],
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
 });
